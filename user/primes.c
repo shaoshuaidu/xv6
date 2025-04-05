9b5d911c7bacae47d98b33a28d98382cfcb19f06
shaoshuaidu <shaoshuaidu@gmail.com>
2025-04-05 21:28:45 +0000

    this is homework for prime

---
+++ user/primes.c
@@ -0,0 +1,61 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void seive(int pleft[2]){
+    int p;
+    read(pleft[0], &p, sizeof(p));
+    if(p==-1){
+        exit(0);
+    }
+    printf("prime %d\n", p);
+
+    int pright[2];
+    pipe(pright);
+
+    if(fork()==0){
+        close(pright[1]);
+        close(pleft[0]);
+        seive(pright);
+    }else{
+        close(pright[0]);
+
+        int buf;
+        while(read(pleft[0], &buf, sizeof(buf))&& buf !=-1){
+            if(buf%p != 0){
+                write(pright[1],&buf, sizeof(buf));
+            }
+        }
+
+        buf = -1;
+        //write data to the pipe, id, value, length of the value
+        write(pright[1], &buf, sizeof(buf));
+
+        wait(0);
+        exit(0);
+    }
+}
+
+
+int main(int argc, char **argv){
+    int input_pipe[2];// create an array for pipe, length 2
+    pipe(input_pipe); //create a mem buffer, and store the file descriptors in the array p
+
+    if (fork()==0){// create a child process, when fork returns 0, it means we are in the child process
+        close(input_pipe[1]);
+        seive(input_pipe);
+        exit(0);
+    }else{
+        close(input_pipe[0]);
+        int i;
+        for(i=2; i<35; i++){
+            write(input_pipe[1],&i,sizeof(i));
+        }
+        i = -1;
+        write(input_pipe[1],&i, sizeof(i));
+    }
+
+    wait(0);
+    exit(0);
+    
+}
